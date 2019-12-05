71599e7ddb05fcdac4bbae0c8ca5f8c8754ccf8e
Bruno Parga <brunoparga@gmail.com>
2019-12-05 15:35:37 -0300

    Put favorite jobs first

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
 export const fetchJobs = async () => {
   const jobs = await fetch('https://fave-jobs-api.herokuapp.com/jobs')
     .then((res) => res.json());
-  return jobs;
+  // sort jobs by whether they're favorites. Even though it works,
+  // 'return job2.favorite - job1.favorite' violates my aesthetic preferences.
+  // Since sort() mutates the array, I'm copying it for functional principles.
+  return Array
+    .from(jobs)
+    .sort((job1, job2) => (job1.favorite >= job2.favorite ? -1 : 1));
 };
 
 // This is here to appease the gods of linting
