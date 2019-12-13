948faaed435b39738ef78567688c78ac57806509
Bruno Parga <brunoparga@gmail.com>
2019-12-13 11:17:58 -0300

    Updates

---
+++ precache-manifest.42c10c1625b999cfab88d01042f98965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c17859acec29acfb6b2fc89d2c1dee",
+    "url": "/fave-jobs-react/index.html"
+  },
+  {
+    "revision": "db800caa57495eaae120",
+    "url": "/fave-jobs-react/static/css/main.f2b2bfcf.chunk.css"
+  },
+  {
+    "revision": "e62c902316b10166f415",
+    "url": "/fave-jobs-react/static/js/2.b3f8e821.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/fave-jobs-react/static/js/2.b3f8e821.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db800caa57495eaae120",
+    "url": "/fave-jobs-react/static/js/main.a9730227.chunk.js"
+  },
+  {
+    "revision": "ad1618f4d6e72ff940a3",
+    "url": "/fave-jobs-react/static/js/runtime-main.0d48f2f2.js"
+  }
+]);
